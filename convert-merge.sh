f7b4c9a2a84d06ea29a7606f55b8203239d5bdf2
Jason van Zyl <jason@vanzyl.ca>
2017-05-21 13:45:36 -0500

    Complete git repo merging and conversion

--- convert-merge.sh
+++ convert-merge.sh
@@ -4,7 +4,7 @@ dir=`pwd`
 oneops=`pwd`/oneops
 
 rm -f merge.txt > /dev/null 2>&1
-rm -rf merged-repo > /dev/null 2>&1
+rm -rf $oneops > /dev/null 2>&1
 while read project; do
   remote=git@github.com:oneops/${project}.git
   echo "$remote:$project" >> merge.txt
@@ -16,9 +16,14 @@ cp pom.xml.template $oneops
 
 # Provisio files for building distribution archives
 cp -r provisio/* $oneops
+# Oneops Distribution
+cp -r oneops-distribution $oneops
+(cd $oneops; git add .; git commit -m 'Add provisio distribution mechanism.')
 
 # Convert all the Maven POMs to build as a single Maven project
 java -jar oneops-converter.jar $oneops
+rm $oneops/pom.xml.template
+(cd $oneops; git add .; git commit -m 'Align all OneOps pom.xml files.')
 
 (
 cd $oneops
@@ -29,4 +34,10 @@ while read project; do
 done < $dir/oneops.txt
 xmllint --format pom.xml > foo.xml
 mv foo.xml pom.xml
+git add .; git commit -m 'Format all OneOps pom.xml files.'
 )
+
+cp -r .mvn $oneops
+cp mvnw $oneops
+cp mvnw.cmd $oneops
+(cd $oneops; git add .; git commit -m 'Add Maven Wrapper.')
